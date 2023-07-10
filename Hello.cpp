2245edd40cc1836099933b1d5306540a355bb0ec
Smith5476 <139094291+Smith5476@users.noreply.github.com>
2023-07-10 10:37:59 +0700

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{  cout<<"student code : 6606021621186"<<endl;
+   cout<<"student Name : Smith Khumnoiy"<<endl;
+   cout<<"    Division : Informaytion Technology"<<endl;
+   cout<<"        Year : 1"<<endl;
+   cout<<"        Room : RC"<<endl;
+   cout<<"         Sex : Man"<<endl;
+   cout<<"         Age : 18"<<endl;
+
+   system("pause");
+   return(0);
+}
