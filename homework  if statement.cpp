8956322fbdca8d3540b0358c5542ea7021b6f856
Smith5476 <139094291+Smith5476@users.noreply.github.com>
2023-08-09 11:46:03 +0700

    Add files via upload

--- homework  if statement.cpp
+++ homework  if statement.cpp
@@ -2,25 +2,55 @@
 #include <string>
 using namespace std;
 int main()
-{
-	int A=10 ,B=20 ,C=30,sum,com;
-	cout<<"enter A:";
-	cin>>A;
-	cout<<"enter B:";
-	cin>>B;
-	cout<<"enter C:";
-	cin>>C;
-	sum = A+B+C;
-	cout<<"A+B+C = 10 + 20 + 30="<<sum<<endl;
-	cout<<"-------output--------"<<endl;
-	cout<<"enter C=:";
-	cin>>C;
-	cout<<"enter B:";
-	cin>>B;
-	cout<<"enter C:";
-	cin>>C;
-	com = C+B+C;
-	cout<<"C+B+C = 30+20+30 "<<com<<endl;
-    system("pause");
-    return (0);
+{int main() {
+    char char1, char2, char3;
+    int num1, num2, num3, sum;
+
+    cout << "Enter Character (A,B,C) : ";
+    cin >> char1;
+
+    if (char1 == 'A') {
+        num1= 10;
+    } else if (char1 == 'B') {
+        num1 = 20;
+    } else if (char1 == 'C') {
+        num1 = 30;
+    } else {
+        num1 = 0;
+    }
+
+    cout << "Enter Character (A,B,C) : ";
+    cin >> char2;
+
+    if (char2 == 'A') {
+        num2 = 10;
+    } else if (char2 == 'B') {
+        num2 = 20;
+    } else if (char2 == 'C') {
+        num2 = 30;
+    } else {
+         num2 = 0;
+    }
+
+    cout << "Enter Character (A,B,C) : ";
+    cin >> char3;
+
+    if (char3 == 'A') {
+       num3 = 10;
+    } else if (char3 == 'B') {
+        num3 = 20;
+    } else if (char3 == 'C') {
+        num3 = 30;
+    } else {
+        num3 = 0;
+    }
+
+    sum = num1 + num2 + num3;
+
+    cout << "A = " << num1 << " , B = " << num2 << " , C = " << num3 << endl;
+    cout << "A + B + C = " << num1 << " + " << num2 << " + " << num3 << " = " << sum << endl;
+
+	system("pause");
+    return 0;
 }
+
